9c40a0e095b09aec0a831770f30720972e7aa14c
Will Jessop <wjessop@gmail.com>
2022-12-13 20:11:32 -0500

    Add example app (#1)

---
+++ Example App/Example App/BookDetailView.swift
@@ -0,0 +1,61 @@
+// 
+
+import Foundation
+import SwiftUI
+import CoreData
+import CoreDataPlus
+
+struct BookDetailView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @ObservedObject var book: Book
+    
+    public var body: some View {
+        ScrollView {
+            VStack {
+                Text(book.title ?? "No title")
+                    .font(.title)
+                
+                ForEach((book.authors?.allObjects as? [Author] ?? []), id: \.self) { author in
+                    HStack(spacing: 5) {
+                        Text(author.name ?? "No name")
+                        Button("❌", action: {
+                            book.removeFromAuthors(author)
+                        })
+                    }
+                }
+                
+                
+                VStack(spacing: 100) {
+                    Button("Add author\nHaruki Murakami", action: {
+                        let murakami = Author.findOrCreate(column: "name", value: "Haruki Murakami", context: viewContext)
+                        book.addToAuthors(murakami)
+                    })
+                    .buttonStyle(.bordered)
+                    
+                    Button("Add author\nMurakami, J.K., or Millet", action: {
+                        let names = [
+                            "Haruki Murakami",
+                            "J. K. Rowling",
+                            "Lydia Millet"
+                        ]
+                        
+                        book.addToAuthors(Author.findOrCreate(column: "name", value: names.randomElement()!, context: viewContext))
+                    })
+                    .buttonStyle(.bordered)
+                    
+                    Button("Add new fictional author", action: {
+                        book.addToAuthors(Author.findOrCreate(column: "name", value: "Author \(Int.random(in: 1...1000))", context: viewContext))
+                    })
+                    .buttonStyle(.bordered)
+                    
+                    Button("Add new fictional author", action: {
+                        book.addToAuthors(Author.findOrCreate(column: "name", value: "Author \(Int.random(in: 1...1000))", context: viewContext))
+                    })
+                    .buttonStyle(.bordered)
+                }
+                
+                
+            }
+        }
+    }
+}
---
+++ Example App/Example App/Example_AppApp.swift
@@ -0,0 +1,30 @@
+//
+//  Example_AppApp.swift
+//  Example App
+//
+//  Created by Will Jessop on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Example_AppApp: App {
+    let persistenceController = PersistenceController.shared
+    @Environment(\.scenePhase) private var phase
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: phase) { newPhase in
+            switch newPhase {
+            case .background:
+                try! persistenceController.container.viewContext.save()
+            default:
+                break
+            }
+        }
+    }
+    
+}
