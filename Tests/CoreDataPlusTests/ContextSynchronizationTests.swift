6fdd7c07e6cbaee9834dc463b57094e0f23b96bf
Will Jessop <wjessop@gmail.com>
2023-02-28 13:46:07 -0500

    Structure refactor

--- Tests/CoreDataPlusTests/ContextSynchronizationTests.swift
+++ Tests/CoreDataPlusTests/ContextSynchronizationTests.swift
@@ -13,13 +13,13 @@ final class ContextSynchronizationTests: BaseTestCase {
         c.clearAll()
         
         b.performAndWait {
-            let book = Book.findOrCreate(id: "hello", context: b)
-            let book2 = Book.findOrCreate(id: "hello2", context: b)
+            _ = Book.findOrCreate(id: "hello", context: b)
+            _ = Book.findOrCreate(id: "hello2", context: b)
         }
         
         await b.perform(schedule: .enqueued, { [b] in
-            let book3 = Book.findOrCreate(id: "hello3", context: b)
-            let book4 = Book.findOrCreate(id: "hello4", context: b)
+            _ = Book.findOrCreate(id: "hello3", context: b)
+            _ = Book.findOrCreate(id: "hello4", context: b)
         })
         
         XCTAssertEqual(try c.count(for: Book.fetchRequest()), 0)
@@ -39,13 +39,13 @@ final class ContextSynchronizationTests: BaseTestCase {
         c.clearAll()
         
         b.performAndWait {
-            let nyc = City.findOrCreate(id: "nyc-1", context: b)
-            let enUS = Language.findOrCreate(column: "langCode", value: "en-us", context: b)
+            _ = City.findOrCreate(id: "nyc-1", context: b)
+            _ = Language.findOrCreate(column: "langCode", value: "en-us", context: b)
         }
         
         await b.perform(schedule: .enqueued, { [b] in
-            let ldn = City.findOrCreate(id: "ldn-1", context: b)
-            let jp = Language.findOrCreate(column: "langCode", value: "jp", context: b)
+            _ = City.findOrCreate(id: "ldn-1", context: b)
+            _ = Language.findOrCreate(column: "langCode", value: "jp", context: b)
         })
         
         XCTAssertEqual(try c.count(for: Language.fetchRequest()), 0)
@@ -70,14 +70,14 @@ final class ContextSynchronizationTests: BaseTestCase {
         c.clearAll()
         
         b.performAndWait {
-            let nyc = City.findOrCreate(id: "nyc-1", context: b)
-            let enUS = Language.findOrCreate(column: "langCode", value: "en-us", context: b)
+            _ = City.findOrCreate(id: "nyc-1", context: b)
+            _ = Language.findOrCreate(column: "langCode", value: "en-us", context: b)
             let jp1 = Language.findOrCreate(column: "langCode", value: "jp", context: b)
             jp1.name = "Japanese 1"
         }
         
         await b.perform(schedule: .enqueued, { [b] in
-            let nyc2 = City.findOrCreate(id: "nyc-1", context: b)
+            _ = City.findOrCreate(id: "nyc-1", context: b)
             let jp = Language.findOrCreate(column: "langCode", value: "jp", context: b)
             jp.name = "Japanese 2"
         })
